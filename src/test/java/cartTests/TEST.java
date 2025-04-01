5cbc5bbc5014693cc72ed771858d8dc4c533853e
Pooja <poojaprabhulingkalyani@gmail.com>
2025-04-01 11:35:50 +0530

    More changes for SCM

--- src/test/java/cartTests/TEST.java
+++ src/test/java/cartTests/TEST.java
@@ -3,5 +3,6 @@ package cartTests;
 public class TEST {
 public static void main(String[] args) {
 	System.out.println("Hi, changes made in script for SCM Poll");
+	System.out.println("More changes");
 }
 }
